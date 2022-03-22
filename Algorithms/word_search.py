02dec90fea221d0bb695b79f18087293945ab18e
Jared Dyreson <jared.dyreson@gmail.com>
2022-03-21 23:43:25 -0700

    this is just for version control

--- Algorithms/word_search.py
+++ Algorithms/word_search.py
@@ -2,10 +2,13 @@
 
 import typing
 import pathlib
+import itertools
+import types
 
 # Directions we can move
 
-movements = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+movements = [(-1, -1), (-1, 0), (-1, 1), (0, -1) ,(0, 1), (1, -1), (1, 0), (1, 1)]
+# movements = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
 class GenericSolution:
@@ -15,23 +18,20 @@ class GenericSolution:
         self.path = []
 
         with open(input_, "r", encoding="utf-8") as fil_ptr:
-            content = [x.strip() for x in fil_ptr.readlines()]
+            contents = (_.strip() for _ in fil_ptr.readlines())
 
-        # Get the dimensions and convert them into integers
-
-        self.ROW, self.COL = map(int, content[0].split(" "))
-
-        self.letters, self.mode, self.words = (
-            list(
-                map(list, content[1 : self.COL + 1])
-            ),  # get all the characters into a grid
-            content[self.COL + 1],
-            content[self.COL + 3 :],
-        )
+        self.COL, self.ROW = map(int, next(contents).split(" "))
+        self.letters = list(itertools.islice(contents, self.COL))
+        # self.letters = map(list, itertools.islice(contents, self.COL))
+        print(self.letters)
+        self.mode = next(contents)
+        self.N = int(next(contents))
+        self.words = list(itertools.islice(contents, self.N))
 
         self._cached_x_domain, self._cached_y_domain = map(
             list, (range(0, self.ROW), range(0, self.COL))
         )
+
 
     def out_of_bounds(self, i: int, j: int) -> bool:
         """
@@ -44,8 +44,14 @@ class GenericSolution:
                 inside (bool): True if it is inside, False otherwise
         """
 
-        return i not in self._cached_x_domain or j not in self._cached_y_domain
+        return (
+            i < 0
+            or i >= self.ROW
+            or j < 0
+            or j >= self.COL
+        )
 
+        # return i not in self._cached_x_domain or j not in self._cached_y_domain
 
 class NoWrapSolution(GenericSolution):
     """Implementation of no wrapping solution"""
@@ -109,6 +115,73 @@ class NoWrapSolution(GenericSolution):
             for j in range(0, self.COL):
                 if self.depth_first_search(i, j, word, 0):
                     print(f"[FOUND]: {word} => {self.path[0]} {self.path[-1]}")
+                    return True
+        print(f"[NOT FOUND]: {word}")
+        return False
+
+class WrappingSolution(GenericSolution):
+
+    def depth_first_search(
+        self,
+        i: int,
+        j: int,
+        word: str,
+        position: int,
+    ) -> bool:
+        print(f'i: {i}, j: {j}, position: {position}')
+        """
+        Search the grid recursively until we hit a condition where we
+        cannot continue processing
+
+        - Horizontal direction under/over flows the grid
+        - Vertical direction under/over flows the grid
+        - Current grid element does not match the current position in the word
+
+            Parameters:
+                i (int): horizontal position in the matrix
+                j (int): vertical position in the matrix
+                word (str): current word we are searching for
+                position (int): position inside the string we are searching for
+            Returns:
+                exist (bool): conditional return depending on which edge case we're dealig with
+        """
+
+        if position == len(word):
+            # print(position == word[position])
+            print(i, j)
+            return True
+
+        if not (char := self.get(i, j, position, word)):
+            return False
+        print(char)
+        # if not self.get(i, j):
+            # return False
+
+        for x, y in movements:
+            if self.depth_first_search(i + x, j + y, word, position + 1):
+                self.path.insert(0, (i, j))
+                # self.path.append((i, j))
+                return True
+
+        return False
+
+    def get(self, i: int, j: int, position: int, word: str) -> bool:
+        return types.NoneType if self.out_of_bounds(i, j) else self.letters[i][j] != word[position]
+
+    def exist(self, word: str, extension: typing.List[typing.Tuple[int, int]]):
+        """
+        Check for it's presence
+
+            Parameters:
+                word (str): the word you want to search for
+            Returns:
+                presence (bool): is in the word search
+        """
+        for i in range(0, self.ROW):
+            for j in range(0, self.COL):
+                if self.depth_first_search(i, j, word, 0):
+                    # print(f"[FOUND]: {word} => {self.path[0]} {self.path[-1]}")
+                    print(f"[FOUND]: {word} => {self.path}")
                     return True
         print(f"[NOT FOUND]: {word}")
         return False
--- UnitTests/WordSearch/test_wrap.py
+++ UnitTests/WordSearch/test_wrap.py
@@ -3,7 +3,7 @@
 import unittest
 import pathlib
 
-from Algorithms.word_search import NoWrapSolution
+from Algorithms.word_search import WrappingSolution
 
 
 class WrapTest(unittest.TestCase):
@@ -14,7 +14,7 @@ class WrapTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.solution = NoWrapSolution(pathlib.Path("inputs/word_search_02.txt"))
+        cls.solution = WrappingSolution(pathlib.Path("inputs/word_search_02.txt"))
 
     def test_mode(self):
         self.assertTrue(self.solution.mode == "WRAP")
@@ -22,6 +22,7 @@ class WrapTest(unittest.TestCase):
     def test_words(self):
         """Place holder"""
         for word in self.solution.words:
+            print(f"[INFO] Checking word: {word}")
             self.solution.exist(word, [])
             # print(self.solution.path)
             self.solution.path.clear()
